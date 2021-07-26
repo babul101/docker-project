7473f80a8de2fcc0961d0904d9c84b0aee99e9ed
unknown <babuldeb101@gmail.com>
2021-07-26 11:36:18 +0000

    created docker files for both ends and docker-compose yml file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const API_URL = process.env.REACT_APP_API_URL || "localhost";
+    const API_PORT = process.env.REACT_APP_API_PORT || "3001";
+    const API_BASE_ADDRESS = `http://${API_URL}:${API_PORT}`;
+    console.log("Request on address ", API_BASE_ADDRESS);
+    const fetchData = async () => {
+      const result = await axios(`${API_BASE_ADDRESS}/test`);
+      setResponse(result.data);
+    };
+    fetchData();
+    return () => {};
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          the response is {response}
         </a>
       </header>
     </div>
